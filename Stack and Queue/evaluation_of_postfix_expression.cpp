b9263610faf81b5993fe65c3add594ddb852171a
kushalnareda <bb1211214@iitd.ac.in>
2024-06-06 23:37:41 +0530

    Create evaluation_of_postfix_expression.cpp

---
+++ Stack and Queue/evaluation_of_postfix_expression.cpp
@@ -0,0 +1,34 @@
+#include iosstream
+
+
+int evaluatePostfix(string S)
+    {
+        // Your code here
+        stack<int> st;
+        for (int i = 0 ; i<S.size() ; i++){
+            if (S[i]>='0' && S[i]<='9'){
+                st.push(S[i]-'0');
+            }
+            else {
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                
+                switch (S[i]){
+                    case '+':
+                        st.push(op2+op1);
+                        break;
+                    case '-':
+                        st.push(op2-op1);
+                        break;
+                    case '*':
+                        st.push(op2*op1);
+                        break;
+                    case '/':
+                        st.push(op2/op1);
+                        break;
+                }
+            }
+        } return st.top();
+    }
